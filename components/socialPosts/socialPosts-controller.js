7195f95682dcd1b2e99132062c91d56b120a6963
Leslie Borst <leslieaborst@gmail.com>
2018-10-17 14:35:29 -0400

    a lab using n=angular to create a blog post app

---
+++ components/socialPosts/socialPosts-controller.js
@@ -0,0 +1,30 @@
+"use strict";
+{
+    angular.module('app')
+        .controller('SocialPostsController', function() {
+            const $ctrl = this;
+
+            $ctrl.posts = [
+                {title: "Cat", thought: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed purus velit, aliquam quis velit non, dictum luctus ipsum."},
+                {title: "Dog", thought: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed purus velit, aliquam quis velit non, dictum luctus ipsum."},
+                {title: "Mouse", thought: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed purus velit, aliquam quis velit non, dictum luctus ipsum."},
+                {title: "Fish", thought: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed purus velit, aliquam quis velit non, dictum luctus ipsum."},
+            ];
+
+            $ctrl.selectColor = function (post) {
+                $ctrl.item = post;
+            };
+
+            $ctrl.addPost = function(newpost) {
+                $ctrl.posts.unshift(newpost);
+                $ctrl.showme = false;
+                $ctrl.hideme = false;
+            }
+
+            $ctrl.openForm = function() {
+                $ctrl.showme = true;
+                $ctrl.hideme = true;
+            }
+
+        });
+}
---
+++ components/socialPosts/socialPosts.js
@@ -0,0 +1,8 @@
+"use strict";
+{
+    angular.module('app') 
+        .component('socialPosts', {
+            controller: 'SocialPostsController',
+            templateUrl: 'components/socialPosts/socialPosts.html'
+        });
+}
